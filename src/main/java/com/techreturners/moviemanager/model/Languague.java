bab61c08aae91e62dd14ee0991c2f48013b366f9
punkason <punkason@gmail.com>
2022-02-24 21:04:52 +0000

    Create Language enum class

--- src/main/java/com/techreturners/moviemanager/model/Languague.java
+++ src/main/java/com/techreturners/moviemanager/model/Languague.java
@@ -1,4 +1,12 @@
 package com.techreturners.moviemanager.model;
 
 public enum Languague {
+    English,
+    Chinese,
+    Russian,
+    Indian,
+    French,
+    Italian,
+    Korean,
+    Japanese,
 }

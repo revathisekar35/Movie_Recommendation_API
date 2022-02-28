22e8b56403f0a4028cf5cef3303cdfb722cfd6b7
revathisekar35 <revathisekar2010@gmail.com>
2022-02-28 13:07:47 +0000

    Refactor : Refactoring the code in ErrorMessage class

--- src/main/java/com/techreturners/moviemanager/exception/ErrorMessage.java
+++ src/main/java/com/techreturners/moviemanager/exception/ErrorMessage.java
@@ -2,32 +2,15 @@ package com.techreturners.moviemanager.exception;
 
 import java.util.Date;
 
-public class ErrorMessage { 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessage {
 	private int statusCode;
 	private Date timestamp;
 	private String message;
 	private String description;
 
-	public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
-		this.statusCode = statusCode;
-		this.timestamp = timestamp;
-		this.message = message;
-		this.description = description;
-	}
-
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public String getDescription() {
-		return description;
-	}
 }
